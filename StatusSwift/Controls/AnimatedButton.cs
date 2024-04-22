b2c1a149d1f117822661d3a6d58ea57eb11d557a
Moritz Reis <clfaster@outlook.com>
2024-04-22 18:41:46 +0200

    Add nuget for minimize to systemtray

---
+++ StatusSwift/Controls/AnimatedButton.cs
@@ -0,0 +1,52 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Maui;
+using SkiaSharp.Views.Maui.Controls;
+
+namespace StatusSwift.Controls;
+
+public class AnimatedButton : SKCanvasView
+{
+    private bool isPressed = false;
+    private SKColor normalColor = SKColors.Blue;
+    private SKColor pressedColor = SKColors.LightBlue;
+
+    public AnimatedButton()
+    {
+        EnableTouchEvents = true;
+
+        PaintSurface += OnPaintSurface;
+        Touch += OnTouch;
+    }
+
+    private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+    {
+        SKCanvas canvas = e.Surface.Canvas;
+        canvas.Clear();
+
+        SKPaint paint = new SKPaint();
+        paint.Color = isPressed ? pressedColor : normalColor;
+        paint.IsAntialias = true;
+
+        // Draw the button
+        SKRect rect = new SKRect(0, 0, 100, 40);
+        canvas.DrawRoundRect(rect, 10, 10, paint);
+    }
+
+    private void OnTouch(object sender, SKTouchEventArgs e)
+    {
+        switch (e.ActionType)
+        {
+            case SKTouchAction.Pressed:
+                isPressed = true;
+                InvalidateSurface();
+                break;
+            case SKTouchAction.Released:
+                isPressed = false;
+                InvalidateSurface();
+                // You can add more logic here for handling button clicks
+                break;
+        }
+
+        e.Handled = true;
+    }
+}
--- StatusSwift/ViewModel/MainViewModel.cs
+++ StatusSwift/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using H.NotifyIcon;
 using Microsoft.Extensions.Logging;
 using StatusSwift.Services;
 
@@ -19,5 +20,34 @@ public partial class MainViewModel(ILogger<MainViewModel> logger, IStatusSwiftSe
         ButtonText = _statusSwiftActive ? "Disable StatusSwift" : "Enable StatusSwift";
         logger.LogInformation("State switched to: {ButtonText}", ButtonText);
         statusSwiftService.ToggleTimer(_statusSwiftActive);
+    }
+    
+    
+    private bool IsWindowVisible { get; set; } = true;
+    
+    [RelayCommand]
+    public void ShowHideWindow()
+    {
+        var window = Application.Current?.MainPage?.Window;
+        if (window == null)
+        {
+            return;
+        }
+
+        if (IsWindowVisible)
+        {
+            window.Hide();
+        }
+        else
+        {
+            window.Show();
+        }
+        IsWindowVisible = !IsWindowVisible;
+    }
+
+    [RelayCommand]
+    public void ExitApplication()
+    {
+        Application.Current?.Quit();
     }
 }
